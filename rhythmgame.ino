0a031973143f67b555b710d54504f51c74fb14a8
Electrum9 <vikram.bhagavatula@gmail.com>
2020-02-19 11:39:52 -0800

    reorganized a little

--- rhythmgame.ino
+++ rhythmgame.ino
@@ -1,26 +1,15 @@
 const int analogPin = 0; // read from multiplexer using analog input 0
 const int strobePin = 2; // strobe is attached to digital pin 2
 const int resetPin = 3;  // reset is attached to digital pin 3
+int avg_value = 0;
+int counter = 0;
+boolean beat;
 
 //const long read_freq = 1000;
 //long next_read_freq;
 
 int spectrumValue[7]; // array to hold a2d values
 
-void setup()
-{
-  pinMode(analogPin, INPUT);
-  pinMode(strobePin, OUTPUT);
-  pinMode(resetPin, OUTPUT);
-  analogReference(DEFAULT);
-  
-  digitalWrite(resetPin, LOW);
-  digitalWrite(strobePin, HIGH);
-  
-  Serial.begin(9600);
-  //long t0 = millis();
-  //next_read_freq = t0;
-}
 void readspectrum(){
   digitalWrite(resetPin, HIGH);
   digitalWrite(resetPin, LOW);
@@ -36,8 +25,8 @@ void readspectrum(){
       digitalWrite(strobePin, HIGH);
     }
 }
-void printspectrum(){
-  for(int i = 0; i < 7; i++)
+void printspectrum(int max_index){
+  for(int i = 0; i < max_index; i++)
     {
       Serial.print(spectrumValue[i]);
       Serial.print(" ");
@@ -45,8 +34,56 @@ void printspectrum(){
   Serial.print("\n");
 }
 
+int running_avg(int number){
+  
+  
+  avg_value += spectrumValue[0];
+  if (counter == number){
+    avg_value /= number;
+    counter = 0;
+    return avg_value; 
+  }
+  else{
+    counter+=1;
+  }
+}
+
+void bpm(){
+  if (spectrumValue[0] >= avg_value){
+     beat = true;
+  }
+}
+
+void setup()
+{
+  pinMode(analogPin, INPUT);
+  pinMode(strobePin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
+  analogReference(DEFAULT);
+  
+  digitalWrite(resetPin, LOW);
+  digitalWrite(strobePin, HIGH);
+  
+  Serial.begin(9600);
+  //long t0 = millis();
+  //next_read_freq = t0;
+  /*
+  long int start_time = millis();
+
+  long int duration = 300000; 
+  */ 
+
+  
+}
+
 void loop()
 {
+  //readspectrum();
+  //printspectrum();
+
+  
   readspectrum();
-  printspectrum();
+  printspectrum(7);
+ 
+  
 }
