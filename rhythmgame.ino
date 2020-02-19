de2ff59dfa0f8e034b24dd1e0bbd102a41292e1c
Sean Tseng <31013596+seanemo@users.noreply.github.com>
2020-02-19 09:31:46 -0800

    Add files via upload

--- rhythmgame.ino
+++ rhythmgame.ino
@@ -1,6 +1,9 @@
-int analogPin = 0; // read from multiplexer using analog input 0
-int strobePin = 2; // strobe is attached to digital pin 2
-int resetPin = 3;  // reset is attached to digital pin 3
+const int analogPin = 0; // read from multiplexer using analog input 0
+const int strobePin = 2; // strobe is attached to digital pin 2
+const int resetPin = 3;  // reset is attached to digital pin 3
+
+//const long read_freq = 1000;
+//long next_read_freq;
 
 int spectrumValue[7]; // array to hold a2d values
 
@@ -15,12 +18,13 @@ void setup()
   digitalWrite(strobePin, HIGH);
   
   Serial.begin(9600);
+  //long t0 = millis();
+  //next_read_freq = t0;
 }
-
-void loop()
-{
+void readspectrum(){
   digitalWrite(resetPin, HIGH);
   digitalWrite(resetPin, LOW);
+  delayMicroseconds(75);
   
   for (int i = 0; i < 7; i++)
     {
@@ -31,10 +35,18 @@ void loop()
       //Serial.println(analogRead(analogPin));
       digitalWrite(strobePin, HIGH);
     }
+}
+void printspectrum(){
   for(int i = 0; i < 7; i++)
     {
       Serial.print(spectrumValue[i]);
       Serial.print(" ");
     }
   Serial.print("\n");
+}
+
+void loop()
+{
+  readspectrum();
+  printspectrum();
 }
