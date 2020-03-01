d156ce598f8591ee4b98b162152a55950ac24f3c
Sean Tseng <seantseng@ucsb.edu>
2020-02-29 22:28:18 -0800

    maybe works now

--- rhythmgame.ino
+++ rhythmgame.ino
@@ -9,7 +9,7 @@ const int resetPin = 3;  // reset is attached to digital pin 3
 #define RUNSUM_LENGTH 5
 
 int spectrumValue[7]; // array to hold a2d values
-int runningSumValues[SPECVAL_LENGTH][5]; //array to hold past values for running sum
+int runningSumValues[SPECVAL_LENGTH][RUNSUM_LENGTH]; //array to hold past values for running sum
 int writeIndex; // keeps track of what place to write future values 
 
 
@@ -41,59 +41,61 @@ void setup()
 }
 
 void readspectrum(){
-  digitalWrite(resetPin, HIGH);
+  digitalWrite(resetPin, HIGH); //resets current frequency band_index to lowest frequency
   digitalWrite(resetPin, LOW);
   delayMicroseconds(75);
   
   for (int i = 0; i < 7; i++)
     {
       delayMicroseconds(36); 
-      digitalWrite(strobePin, LOW);
+      digitalWrite(strobePin, LOW); //tells chip to take values of current frequency band_index
       delayMicroseconds(36); 
-      spectrumValue[i] = analogRead(analogPin);
+      spectrumValue[i] = analogRead(analogPin);//records spectrum value in array
       //Serial.println(analogRead(analogPin));
-      digitalWrite(strobePin, HIGH);
+      digitalWrite(strobePin, HIGH); //closes current frequency band_index and goes to the next band_index
     }
 }
-void printspectrum(int max_index, bool cumulative){
+
+void printspectrum(int band_index, bool cumulative = true){ //takes an integer value band_index that tells what frequency values to read, also takes in cumulative, which gathers all the lower frequencies underneath if true
   if(cumulative) {
-  for(int i = 0; i < max_index; i++)
-    {
-      Serial.print(spectrumValue[i]);
-      Serial.print(", ");
-    }
-  Serial.print("\n");
+    for(int i = 0; i < band_index; i++)//prints out band_indexs
+      {
+        Serial.print(spectrumValue[i]);
+        Serial.print(", ");
+      }
+    Serial.print("\n");
+  }
+
+  else {
+    Serial.println(spectrumValue[band_index]); 
+  }
 }
 
-else {
-  Serial.println(spectrumValue[max_index]); 
-}
-}
 
+void updateRunningSum(int band_index, bool curr_beat) {
 
-void updateRunningSum(int band, bool curr_beat) {
-
-    int curr_specval = spectrumValue[band]; 
+    int curr_specval = spectrumValue[band_index]; 
    
     if(curr_beat) {
         for(int i = 0; i < RUNSUM_LENGTH; i++) {
-            runningSumValues[band][i] = curr_specval;
-    }
+            runningSumValues[band_index][i] = curr_specval;
+        }
     }
 
     else {
-        runningSumValues[band][writeIndex] = curr_specval; 
+        runningSumValues[band_index][writeIndex] = curr_specval; 
         writeIndex++;
+        if (writeIndex > RUNSUM_LENGTH - 1){
+          writeIndex = 0;
+        }
     }
-    
-
 }
 
 
-int getThreshold(int band) {
+int getThreshold(int band_index) {
     int sum;
     for(int i = 0; i < RUNSUM_LENGTH; i++) {
-        sum += runningSumValues[band][i]; 
+        sum += runningSumValues[band_index][i]; 
     }
 
     return sum; 
@@ -102,11 +104,31 @@ int getThreshold(int band) {
 
 void detectBeat() {
   for(int i = 0; i < SPECVAL_LENGTH; i++) {
-    beats[i] = (spectrumValue[i] > noise_threshold) && (5*spectrumValue[i] >= getThreshold(i));
+    beats[i] = (spectrumValue[i] > noise_threshold) && (RUNSUM_LENGTH*spectrumValue[i] >= getThreshold(i));
     //Serial.println(beats[i]); 
-    updateRunningSum(beats[i], spectrumValue[i]);
+    updateRunningSum(i, beats[i]);
   }
 }
+
+void printbeat(int band_index, bool cumulative=true){
+  if(cumulative) {
+  for(int i = 0; i < band_index; i++) {
+
+    Serial.print(beats[i]*(i+1)); //mul by i+1 to separate pulses out for each band_index
+
+    Serial.print(", "); 
+
+    }
+  }
+  else {
+    Serial.print(beats[band_index]); //mul by i+1 to separate pulses out for each band_index
+
+    }
+
+    Serial.print("\n");
+  }
+
+  
 
 void loop()
 {
@@ -116,11 +138,8 @@ void loop()
 
   // Test Code
 
-  for(int i = 0; i < 7; i++) {
-    Serial.print(beats[i]); //mul by i+1 to separate pulses out for each band
-    Serial.print(", "); 
-  }
-  Serial.print("\n"); 
+  //printspectrum(7);
+  printbeat(3, true);
 
  // End Test Code 
   
