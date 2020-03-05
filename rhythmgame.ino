df30cafa44b963343aa97e1e9f2ccc581009a876
Sean Tseng <seantseng@ucsb.edu>
2020-03-04 16:16:55 -0800

    ledmatrix

--- rhythmgame.ino
+++ rhythmgame.ino
@@ -1,6 +1,22 @@
 const int analogPin = 0; // read from multiplexer using analog input 0
 const int strobePin = 2; // strobe is attached to digital pin 2
 const int resetPin = 3;  // reset is attached to digital pin 3
+const int row1 = 22;
+const int row2 = 23;
+const int row3 = 24;
+const int row4 = 25;
+const int row5 = 26;
+const int row6 = 27;
+const int row7 = 28;
+const int row8 = 36;
+const int col1 = 49;
+const int col2 = 48;
+const int col3 = 47;
+const int col4 = 46;
+const int button1 = 53;
+const int button2 = 52;
+const int button3 = 51;
+const int button4 = 50;
 
 
 //const long read_freq = 1000;
@@ -11,6 +27,7 @@ const int resetPin = 3;  // reset is attached to digital pin 3
 int spectrumValue[7]; // array to hold a2d values
 int runningSumValues[SPECVAL_LENGTH][RUNSUM_LENGTH]; //array to hold past values for running sum
 int writeIndex; // keeps track of what place to write future values 
+long int pastrow;
 
 
 //int curr_threshold = 0; 
@@ -23,6 +40,22 @@ void setup()
   pinMode(analogPin, INPUT);
   pinMode(strobePin, OUTPUT);
   pinMode(resetPin, OUTPUT);
+  pinMode(row1,OUTPUT);
+  pinMode(row2,OUTPUT);
+  pinMode(row3,OUTPUT);
+  pinMode(row4,OUTPUT);
+  pinMode(row5,OUTPUT);
+  pinMode(row6,OUTPUT);
+  pinMode(row7,OUTPUT);
+  pinMode(row8,OUTPUT);
+  pinMode(col1,OUTPUT);
+  pinMode(col2,OUTPUT);
+  pinMode(col3,OUTPUT);
+  pinMode(col4,OUTPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
   analogReference(DEFAULT);
   
   digitalWrite(resetPin, LOW);
@@ -36,7 +69,9 @@ void setup()
 
   long int duration = 300000; 
   */ 
-
+  PORTA = 64;
+  PORTL = 0;
+  
   
 }
 
@@ -113,25 +148,30 @@ void detectBeat() {
 void printbeat(int band_index, bool cumulative=true){
   if(cumulative) {
   for(int i = 0; i < band_index; i++) {
-
     Serial.print(beats[i]*(i+1)); //mul by i+1 to separate pulses out for each band_index
-
     Serial.print(", "); 
-
     }
   }
   else {
     Serial.print(beats[band_index]); //mul by i+1 to separate pulses out for each band_index
-
     }
-
     Serial.print("\n");
+}
+  
+void ledrowdrop(){
+  if (millis()- pastrow > 250){
+    PORTA /= 2;
+    pastrow = millis();
+    if (PORTA == 0){
+      PORTA = 64;
+    }
   }
-
+  
+}
   
 
-void loop()
-{
+void loop(){
+
 
   readspectrum(); 
   detectBeat(); 
@@ -142,6 +182,6 @@ void loop()
   printbeat(3, true);
 
  // End Test Code 
-  
+ ledrowdrop(); 
   
 }
